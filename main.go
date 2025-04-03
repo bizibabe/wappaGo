f6352c1566d60a8dc40fbdfe30f3f41ce0894113
Bizi <60073248+bizibabe@users.noreply.github.com>
2025-04-03 16:48:01 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 	"log"
 	"os"
 
-	"github.com/EasyRecon/wappaGo/cmd"
-	"github.com/EasyRecon/wappaGo/structure"
-	"github.com/EasyRecon/wappaGo/technologies"
+	"github.com/bizibabe/wappaGo/cmd"
+	"github.com/bizibabe/wappaGo/structure"
+	"github.com/bizibabe/wappaGo/technologies"
 )
 
 func main() {
